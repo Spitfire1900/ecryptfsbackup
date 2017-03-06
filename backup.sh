25b1f71a615f00aa211edf78fe85f6796880702f
Kyle Gottfried <Kyle.Gottfried@outlook.com>
2017-03-05 22:26:32 -0500

    Else BACKUP_STYLE is diff

--- backup.sh
+++ backup.sh
@@ -6,11 +6,14 @@ function getBackupStyle() {
 
   if [[ $TODAY -eq '7' ]]; then
     BACKUP_STYLE='full'
-  elif [[ $(find . -regex './l1backup_[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9].tar' -mtime -7 2> /dev/null) ]]; then
+  elif [[ -z $(find . -regex './l1backup_[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9].tar' -mtime -7 2> /dev/null) ]]; then
     BACKUP_STYLE='full'
+  else
+    BACKUP_STYLE='diff'
   fi
 }
 
 # main
 cd /mnt || exit
+getBackupStyle
 echo $BACKUP_STYLE
