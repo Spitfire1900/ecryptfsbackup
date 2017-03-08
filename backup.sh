e879fd8145d6016b583f5bbc9a1d457d780e26ef
Kyle Gottfried <Kyle.Gottfried@outlook.com>
2017-03-07 20:38:23 -0500

    Use local and readonly variables

--- backup.sh
+++ backup.sh
@@ -2,19 +2,25 @@
 set -eux
 
 function getBackupStyle() {
-  BACKUP_STYLE=''
+  local BACKUP_STYLE
+  local TODAY
   TODAY=$(date +%d)
+  readonly TODAY
 
   if [[ $TODAY -eq '7' ]]; then
     BACKUP_STYLE='full'
+    readonly BACKUP_STYLE
   elif [[ -z $(find . -regex './l1backup_[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9].tar' -mtime -7 2> /dev/null) ]]; then
     BACKUP_STYLE='full'
+    readonly BACKUP_STYLE
   else
     BACKUP_STYLE='diff'
+    readonly BACKUP_STYLE
   fi
+  echo $BACKUP_STYLE
 }
 
 # main
 cd /mnt || exit
-getBackupStyle
+BACKUP_STYLE=$(getBackupStyle)
 echo $BACKUP_STYLE
