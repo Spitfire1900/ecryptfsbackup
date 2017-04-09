d3ca2715ee8c62a5f3867c6e4ba9b7289ae92b73
Kyle Gottfried <Kyle.Gottfried@outlook.com>
2017-04-08 21:52:15 -0400

    Implement retrieval of BACKUP_SETS

--- backup.sh
+++ backup.sh
@@ -20,6 +20,29 @@ function getBackupStyle() {
   echo $BACKUP_STYLE
 }
 
+function getAverageBackupSize() {
+  local BACKUPS; BACKUPS=$(find . -maxdepth 1 -regex './[0-9][0-9][0-9]_.*backup.*_[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9].tar')
+  declare -a BACKUP_SETS
+  local i=0
+  while [ $i -lt 1000 ]; do
+    local iteration; iteration=$i
+    if [ $iteration -lt 10 ]; then
+      iteration=00$i
+    elif [[ $iteration -lt 100  ]]; then
+      iteration=0$i
+    fi
+    for f in $BACKUPS; do
+      if [[ $f =~ .\/$iteration.* ]]; then
+        if [[ -z  ${BACKUP_SETS[i]-} ]]; then
+          BACKUP_SETS[$i]=''
+        fi
+        BACKUP_SETS[$i]=${BACKUP_SETS[i]}' '$f
+      fi
+    done
+    i=$((i + 1))
+  done
+}
+
 function cleanDisk() {
   local AVG_BACKUP_SIZE=$()
   # find . -regex './l1.*.tar' -exec du {} \; 2> /dev/null | awk '{print $1;}' | awk '{ total += $1; count++ }'
